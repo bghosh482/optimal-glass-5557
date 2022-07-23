81698addf77e90df0d9ac38c7c4f6553e649abe8
Nikhil Dharendra <engineerdharendra@gmail.com>
2022-07-23 18:57:48 +0530

    products

---
+++ 3)boat products/scripts/base.js
@@ -0,0 +1,44 @@
+// import navbar from "./components./navbar.js"
+// import navbar from "./components./footer.js"
+
+import  {navbar} from "./components./navbar.js"
+let nav = document.getElementById("navbar")
+nav.innerHTML = navbar()
+
+
+import {footer} from "./components./footer.js"
+let foot = document.getElementById("footer")
+foot.innerHTML = footer()
+    function box1(){
+        window.location.href = "Airdopes.html"
+    }
+    function box2(){
+        window.location.href = "rockerz.html"
+    }
+    function box3(){
+        window.location.href = "watches.html"
+    }
+    function box4(){
+        window.location.href = "wired.html"
+    }
+    function box5(){
+        window.location.href = "speaker.html"
+    }
+    function box6(){
+        window.location.href = "audio.html"
+    }
+    function box7(){
+        window.location.href = "mobile.html"
+    }
+    function box8(){
+        window.location.href = "tws.html"
+    }
+    function box9(){
+        window.location.href = "head1.html"
+    }
+    function box10(){
+        window.location.href = "head2.html"
+    }
+    function box11(){
+        window.location.href = "Head3.html"
+    }
